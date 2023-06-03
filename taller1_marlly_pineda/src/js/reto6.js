8396c1485f0132f7bc96972cf9105b72c8f92717
Marllydpv <marllydanielapinedavillamil@gmail.com>
2023-06-03 16:16:07 -0500

    si

---
+++ taller1_marlly_pineda/src/js/reto6.js
@@ -0,0 +1,8 @@
+
+function mostrarTerminos() {
+  document.getElementById("modal").style.display = "block";
+}
+
+function ocultarTerminos() {
+  document.getElementById("modal").style.display = "none";
+}

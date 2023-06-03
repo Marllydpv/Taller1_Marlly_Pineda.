a0bb8340c358e0ac49926e7fb08ee5f7ae36a62b
Marllydpv <marllydanielapinedavillamil@gmail.com>
2023-06-03 17:04:13 -0500

    comprueba

--- taller1_marlly_pineda/src/js/reto7.js
+++ taller1_marlly_pineda/src/js/reto7.js
@@ -1,57 +1,84 @@
-const formulario = document.getElementById("formulario");
-
-formulario.addEventListener("submit", function(event) {
-  event.preventDefault(); 
-  const numDocumento = document.getElementById("num_documento").value;
-  const nombre = document.getElementById("nombre").value;
-  const apellido = document.getElementById("apellido").value;
-  const fechaNacimiento = new Date(document.getElementById("fecha_nacimiento").value);
-  const correo = document.getElementById("correo").value;
-  const password = document.getElementById("password").value;
-  const comprobarPassword = document.getElementById("comprobar_password").value;
-  const terminos = document.getElementById("terminos");
-
-  if (numDocumento.length < 5) {
-    alert("El número de documento debe tener al menos 5 caracteres.");
-    return false;
-  }
-
-  if (!nombre.match(/^[a-zA-Z0-9]+$/)) {
-    alert("El nombre solo debe contener caracteres alfanuméricos.");
-    return false;
-  }
-
-  if (!apellido.match(/^[a-zA-Z0-9]+$/)) {
-    alert("El apellido solo debe contener caracteres alfanuméricos.");
-    return false;
-  }
-
-  const fechaMinima = new Date("2002-01-01");
-  if (fechaNacimiento < fechaMinima) {
-    alert("Debe ser mayor de 18 años para registrarse.");
-    return false;
-  }
-
-  if (!correo.match(/.+@misena\.edu\.co/)) {
-    alert("El correo electrónico debe ser de dominio @misena.edu.co.");
-    return false;
-  }
-
-  if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{10,}$/)) {
-    alert("El password debe incluir al menos una letra mayúscula, un número, una letra minúscula, un carácter y tener una longitud mínima de 10 caracteres.");
-    return false;
-  }
-
-  if (password !== comprobarPassword) {
-    alert("Los passwords no coinciden.");
-    return false;
-  }
-
-  if (!terminos.checked) {
-    alert("Debe aceptar los Términos de uso para registrarse.");
-    return false;
-  }
-
-  // Si todos los campos son válidos, enviar el formulario
-  formulario.submit();
-});
+function validarFormulario() {
+    // Validar campo Tipo de documento
+    var tipoDocumento = document.getElementById("tipo_documento").value;
+    if (tipoDocumento === "") {
+      alert("Por favor, selecciona un tipo de documento.");
+      return false;
+    }
+  
+    // Validar campo No. Documento
+    var numDocumento = document.getElementById("num_documento").value;
+    if (numDocumento === "") {
+      alert("Por favor, ingresa un número de documento.");
+      return false;
+    }
+    if (!/^\d+$/.test(numDocumento)) {
+      alert("El número de documento solo puede contener dígitos.");
+      return false;
+    }
+    if (numDocumento.length < 5) {
+      alert("El número de documento debe tener al menos 5 caracteres.");
+      return false;
+    }
+  
+    // Validar campo Nombre
+    var nombre = document.getElementById("nombre").value;
+    if (nombre === "") {
+      alert("Por favor, ingresa tu nombre.");
+      return false;
+    }
+    if (!/^[a-zA-Z0-9\s]+$/.test(nombre)) {
+      alert("El nombre solo puede contener caracteres alfanuméricos.");
+      return false;
+    }
+  
+    // Validar campo Apellido
+    var apellido = document.getElementById("apellido").value;
+    if (apellido === "") {
+      alert("Por favor, ingresa tu apellido.");
+      return false;
+    }
+    if (!/^[a-zA-Z0-9\s]+$/.test(apellido)) {
+      alert("El apellido solo puede contener caracteres alfanuméricos.");
+      return false;
+    }
+  
+    // Validar campo Fecha de nacimiento
+    var fechaNacimiento = document.getElementById("fecha_nacimiento").value;
+    var fechaNacimientoDate = new Date(fechaNacimiento);
+    var anioLimite = new Date("2002-01-01");
+    if (fechaNacimiento === "" || fechaNacimientoDate < anioLimite) {
+      alert("Debes ser mayor de 18 años para registrarte.");
+      return false;
+    }
+  
+    // Validar campo Correo electrónico
+    var correoElectronico = document.getElementById("correo_electronico").value;
+    if (correoElectronico === "") {
+      alert("Por favor, ingresa tu correo electrónico.");
+      return false;
+    }
+    if (!/\b[A-Za-z0-9._%+-]+@misena\.edu\.co\b/.test(correoElectronico)) {
+      alert("El correo electrónico debe tener el dominio @misena.edu.co.");
+      return false;
+    }
+  
+    // Validar campo Contraseña
+    var contrasena = document.getElementById("contrasena").value;
+    var confirmarContrasena = document.getElementById("confirmar_contrasena").value;
+    if (contrasena === "" || confirmarContrasena === "") {
+      alert("Por favor, ingresa una contraseña y confírmala.");
+      return false;
+    }
+    if (contrasena !== confirmarContrasena) {
+      alert("La contraseña y su confirmación no coinciden.");
+      return false;
+    }
+    if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{10,}/.test(contrasena)) {
+      alert("La contraseña debe tener al menos una letra mayúscula, una letra minúscula, un número, un carácter especial y ser de al menos 10 caracteres.");
+      return false;
+    }
+}
+  
+    
+  
